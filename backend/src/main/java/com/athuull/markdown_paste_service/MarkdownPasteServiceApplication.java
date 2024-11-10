1473f27b6b5ba19160ae60ea88a31f84dd0f739e
athuull <athulvanil03@gmail.com>
2024-11-10 20:29:03 +0530

    Initial commit

---
+++ backend/src/main/java/com/athuull/markdown_paste_service/MarkdownPasteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.athuull.markdown_paste_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkdownPasteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkdownPasteServiceApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/athuull/markdown_paste_service/MarkdownPasteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athuull.markdown_paste_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkdownPasteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
